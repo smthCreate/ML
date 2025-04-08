65472e28c75f64d91dc6289b00a041efdf12336a
Stasy <ivanenko.stasy@gmail.com>
2025-04-08 19:58:37 +0300

    docker2

--- READme.md
+++ READme.md
@@ -1,2 +1,3 @@
 docker build -t mlmodel .
 docker run -d --name mlmodel-container -p5000:5000 mlmodel
+git
--- ml/app_api.py
+++ ml/app_api.py
@@ -7,7 +7,7 @@ import uvicorn
 app = FastAPI()
 
 # Загрузка модели из файла pickle
-with open('clf_model.pkl', 'rb') as f:
+with open('ml/clf_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Счетчик запросов
@@ -59,4 +59,4 @@ def predict_model(input_data: PredictionInput):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
